fa42d75c8a70492e759a5fcca4b8e7b4b3c0c7a2
Vicky Yadav <amrvickyy@gmail.com>
2025-05-29 08:21:05 +0530

    Add lesson1 code

---
+++ lesson-01/javascript-basics.js
@@ -0,0 +1,49 @@
+// what is javascript?
+// -- A technology that we use to create website
+
+// 3 technology use to create a Website
+// 1.HTML
+// 2.CSS
+// 3.JavaScript
+
+// Basics of JavaScript
+// - giving instruction to a computer(code)
+// - the computer follows our instructions(running the code)
+// - javascript makes website intractive
+
+alert('hello');
+// tells the computer to create a popup and the "hello" text inside the popup
+
+alert('Good job!');
+
+2 + 2
+10 - 3
+
+document.body.innerHTML = 'hello';
+// modify the body
+
+console.log('hello vicky');
+
+
+// Exercise?
+// 1.
+        alert('Good morning');
+// 2.
+        alert('Vicky yadav');
+// 3.
+        10 + 5
+// 4. 
+        20 - 5
+// 5.
+        2 + 2 - 5
+// 6. 
+        document.body.innerHTML = 'Good morning';
+// 7.
+        document.body.innerHTML = 'vicky yadav';
+// 8.
+        10 + 8 + 20
+// 9.
+        100 - 20 - 50 + 200
+// 10.
+        document.body.innerHTML = '';
+        // its show blank body of page
